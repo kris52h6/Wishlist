6bfbb62ebd71fd9ea9763f0ad5108525c877802c
kris52h6 <kris52h6@stud.kea.dk>
2022-04-07 10:31:20 +0200

    Dependencies

--- src/main/java/com/example/wishlist/WishlistApplication.java
+++ src/main/java/com/example/wishlist/WishlistApplication.java
@@ -1,4 +1,4 @@
-package com.projectwishlist;
+package com.example.wishlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
